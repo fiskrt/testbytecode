a3493997f33cc7766be8da1449aefc77bce70346
fiskrt <filipskogh99@hotmail.com>
2019-02-05 23:40:21 +0100

    Initial commit

---
+++ src/A.java
@@ -0,0 +1,39 @@
+
+public class A {
+
+    public static void main(String[] args) {
+
+//
+//        int[] t = {1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1};
+//        System.out.println(isPalindrome(t, 0, t.length-1));
+
+//        System.out.println(isEven(20000));
+    }
+
+
+    public void calcK(int k){
+
+    }
+
+
+//    private static boolean isPalindrome(int[] arr, int startIndex, int endIndex){
+//        if (startIndex >= endIndex)
+//            return true;
+//
+//        return (arr[startIndex] == arr[endIndex]) && isPalindrome(arr, ++startIndex, --endIndex);
+//    }
+//
+//    private static boolean isEven2(int n){
+//
+//    }
+//
+//
+//    private static boolean isEven(int n){
+//        if (n == 0) return true;
+//        return !isEven(n-1);
+//    }
+
+
+
+
+}
---
+++ src/B.java
@@ -0,0 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class B implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        System.out.println("lol");
+    }
+}
